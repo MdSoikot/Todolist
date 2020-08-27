b6954a5e0cae1a6d22416f23f041c26fbf7ef67c
Shakhawat Hosen <shakhawat>
2020-08-27 23:09:51 +0600

    testing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,11 @@
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todolist</title>
-    <link rel="stylesheet" href="../sass/app.css">
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 </head>
 
 <body>
