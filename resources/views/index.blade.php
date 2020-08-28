8a025eeb487672c4e8f3b94e4c1fb171140d7cef
Shakhawat Hosen <shakhawat>
2020-08-28 20:41:39 +0600

    design changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todolist</title>
-    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
